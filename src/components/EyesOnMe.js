95e08a39c8af86036b04f522176bb9b63e2f753c
Kat Tannehill <katetannehill@gmail.com>
2023-10-02 19:17:44 -0600

    Complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+    const focused = () => {
+        console.log('Good!')
+    }
+
+    const blurred = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+  return (
+    <button onFocus={focused} onBlur={blurred}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
-// Code Keypad Component Here
+import React from 'react'
 
 function Keypad (){
+
+    const logPass = () => {
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <input type="password" onChange={logPass}/>
     )
 }
 
